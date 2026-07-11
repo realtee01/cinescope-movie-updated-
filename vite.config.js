ca6de5efaa724375c1d955b675918a89b12e8945
realtee01 <tobilobaakala@gmail.com>
2026-07-11 13:36:27 +0100

    updated

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  envPrefix: ['VITE_', 'TMDB_TOKEN'],
   server: { host: '0.0.0.0', port: 3000, allowedHosts: 'all' }
 })
